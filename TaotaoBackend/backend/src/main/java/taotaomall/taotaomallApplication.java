749bcb7c2f4fbf7d883d09763799b6afbd43b4b2
xx_isxiaoxu <3052307489@qq.com>
2025-05-21 09:38:33 +0800

    fix: 解决冲突并提交本地更改

--- TaotaoBackend/backend/src/main/java/taotaomall/taotaomallApplication.java
+++ TaotaoBackend/backend/src/main/java/taotaomall/taotaomallApplication.java
@@ -2,10 +2,12 @@ package taotaomall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.mybatis.spring.annotation.MapperScan;  // 添加这行导入
 
 @SpringBootApplication
+@MapperScan("taotaomall.dao")
 public class taotaomallApplication {
     public static void main(String[] args) {
-        SpringApplication.run(taotaomallApplication.class,args);
+        SpringApplication.run(taotaomallApplication.class, args);
     }
 }
