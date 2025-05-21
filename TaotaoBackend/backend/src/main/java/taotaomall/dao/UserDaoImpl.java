5e6a49b7dcd3cc2df2604c82af3c45ce1addc785
A-hub520 <1430059918@qq.com>
2025-05-21 09:19:38 +0800

    注册登录开发

---
+++ TaotaoBackend/backend/src/main/java/taotaomall/dao/UserDaoImpl.java
@@ -0,0 +1,21 @@
+package taotaomall.dao;
+
+import taotaomall.model.User;
+
+public class UserDaoImpl implements UserDao{
+
+    @Override
+    public int register(User user) {
+        return 0;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User login(String username, String password) {
+        return null;
+    }
+}
