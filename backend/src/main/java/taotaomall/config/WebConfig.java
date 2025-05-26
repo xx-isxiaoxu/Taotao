cd01816876492c4d0d2b8aa3690c7854a56662b2
xx-isxiaoxu <3052307489@qq.com>
2025-05-26 12:56:31 +0800

    放行图片

--- backend/src/main/java/taotaomall/config/WebConfig.java
+++ backend/src/main/java/taotaomall/config/WebConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class Webconfig implements WebMvcConfigurer {
+public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private JWTInterceptors jwtInterceptors;
 
